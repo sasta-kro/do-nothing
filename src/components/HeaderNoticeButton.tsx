19aa855f24f559f75e26551895219167233a9577
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2026-05-01 20:10:59 +0700

    added the reusable header notice popover component

---
+++ src/components/HeaderNoticeButton.tsx
@@ -0,0 +1,60 @@
+import { useId } from "react";
+
+import { useTimedPopover } from "../hooks/useTimedPopover";
+
+type HeaderNoticeButtonProps = {
+  label: string;
+  noticeTitle: string;
+  noticeBody: string;
+  triggerClassName: string;
+  popoverClassName: string;
+};
+
+export function HeaderNoticeButton({
+  label,
+  noticeTitle,
+  noticeBody,
+  triggerClassName,
+  popoverClassName,
+}: HeaderNoticeButtonProps) {
+  const tooltipId = useId();
+  const {
+    isPopoverVisible,
+    scheduleReveal,
+    revealWithAutoDismiss,
+    hide,
+  } = useTimedPopover({
+    revealDelayMs: 260,
+    autoDismissDelayMs: 2400,
+  });
+
+  return (
+    <div
+      className="header-notice"
+      onMouseEnter={scheduleReveal}
+      onMouseLeave={hide}
+    >
+      <button
+        type="button"
+        className={triggerClassName}
+        aria-describedby={tooltipId}
+        aria-expanded={isPopoverVisible}
+        onFocus={scheduleReveal}
+        onBlur={hide}
+        onClick={revealWithAutoDismiss}
+      >
+        {label}
+      </button>
+      <article
+        id={tooltipId}
+        role="tooltip"
+        className={`${popoverClassName}${
+          isPopoverVisible ? ` ${popoverClassName}--visible` : ""
+        }`}
+      >
+        <p className="header-notice__title">{noticeTitle}</p>
+        <p className="header-notice__body">{noticeBody}</p>
+      </article>
+    </div>
+  );
+}
